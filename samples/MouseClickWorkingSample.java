29b0cf5b45c0f0a7a4409d39320f1ba6953dfe86
eustratiadis-hua <ep.pan.dit@gmail.com>
2013-07-19 09:17:06 +0300

    - Sample and MouseMotionSensor put in comments - Javacv build instructions for Linux added in package "basics"

--- samples/MouseClickWorkingSample.java
+++ samples/MouseClickWorkingSample.java
@@ -1,22 +1,22 @@
-package samples;
-
-import org.scify.jthinkfreedom.reactors.ApplicationLauncherReactor;
-import org.scify.jthinkfreedom.sensors.MouseMotionSensor;
-import org.scify.jthinkfreedom.stimuli.MouseClickStimulus;
-
-/**
- *
- * @author nikos
- */
-public class MouseClickWorkingSample {
-
-    public static void main(String[] args) {
-    	MouseMotionSensor mouseSensor = new MouseMotionSensor();
-		MouseClickStimulus clickStimulus = new MouseClickStimulus(1);
-		ApplicationLauncherReactor appLaunch = new ApplicationLauncherReactor("/usr/bin/gnome-terminal");
-		
-		mouseSensor.addStimulus(clickStimulus);
-		clickStimulus.addSensor(mouseSensor);
-		clickStimulus.addReactor(appLaunch);
-    }
-}
+//package samples;
+//
+//import org.scify.jthinkfreedom.reactors.ApplicationLauncherReactor;
+//import org.scify.jthinkfreedom.sensors.MouseMotionSensor;
+//import org.scify.jthinkfreedom.stimuli.MouseClickStimulus;
+//
+///**
+// *
+// * @author nikos
+// */
+//public class MouseClickWorkingSample {
+//
+//    public static void main(String[] args) {
+//    	MouseMotionSensor mouseSensor = new MouseMotionSensor();
+//	MouseClickStimulus clickStimulus = new MouseClickStimulus(1);
+//	ApplicationLauncherReactor appLaunch = new ApplicationLauncherReactor("/usr/bin/gnome-terminal");
+//		
+//	mouseSensor.addStimulus(clickStimulus);
+//	clickStimulus.addSensor(mouseSensor);
+//	clickStimulus.addReactor(appLaunch);
+//    }
+//}
